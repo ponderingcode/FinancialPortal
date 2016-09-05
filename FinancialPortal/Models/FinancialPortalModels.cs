2ac8669e899babc70f3997f84bcecebde1dc28a9
= <=>
2016-09-05 14:17:26 -0400

    made changes to models prior to running code first migration

--- FinancialPortal/Models/FinancialPortalModels.cs
+++ FinancialPortal/Models/FinancialPortalModels.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FinancialPortal.Models
 {
     public class Budget
     {
+        public Budget()
+        {
+            BudgetItems = new HashSet<BudgetItem>();
+        }
+
         public int Id { get; set; }
         public int HouseholdId { get; set; }
         public string Name { get; set; }
+        public decimal LimitAmount { get; set; }
+        public virtual ICollection<BudgetItem> BudgetItems { get; set; }
     }
 
     public class BudgetItem
@@ -19,8 +27,14 @@ namespace FinancialPortal.Models
 
     public class Household
     {
+        public Household()
+        {
+            Members = new HashSet<ApplicationUser>();
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
+        public virtual ICollection<ApplicationUser> Members { get; set; }
     }
 
     public class Category
@@ -50,7 +64,7 @@ namespace FinancialPortal.Models
         public int Id { get; set; }
         public int AccountId { get; set; }
         public int CategoryId { get; set; }
-        public int EnteredById { get; set; }
+        public string EnteredById { get; set; }
         public DateTime Date { get; set; }
         public string Description { get; set; }
         public string Type { get; set; }
