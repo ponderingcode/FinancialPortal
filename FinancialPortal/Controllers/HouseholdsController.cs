4cb719111f5395b858d3a7893b3a074e5163b7a2
= <=>
2016-09-05 17:02:20 -0400

    added household creation (only administrators can create & delete households)

---
+++ FinancialPortal/Controllers/HouseholdsController.cs
@@ -0,0 +1,126 @@
+﻿using FinancialPortal.Models;
+using System.Data.Entity;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace FinancialPortal.Controllers
+{
+    [Authorize]
+    public class HouseholdsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Households
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Households.ToListAsync());
+        }
+
+        // GET: Households/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Household household = await db.Households.FindAsync(id);
+            if (household == null)
+            {
+                return HttpNotFound();
+            }
+            return View(household);
+        }
+
+        // GET: Households/Create
+        [Authorize(Roles = ApplicationRole.ADMINISTRATOR)]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Households/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name")] Household household)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Households.Add(household);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(household);
+        }
+
+        // GET: Households/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Household household = await db.Households.FindAsync(id);
+            if (household == null)
+            {
+                return HttpNotFound();
+            }
+            return View(household);
+        }
+
+        // POST: Households/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name")] Household household)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(household).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(household);
+        }
+
+        // GET: Households/Delete/5
+        [Authorize(Roles = ApplicationRole.ADMINISTRATOR)]
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Household household = await db.Households.FindAsync(id);
+            if (household == null)
+            {
+                return HttpNotFound();
+            }
+            return View(household);
+        }
+
+        // POST: Households/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Household household = await db.Households.FindAsync(id);
+            db.Households.Remove(household);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
